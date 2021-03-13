3d63c7a4c8399b06484f2c3b1a8f6c7081a4f822
Salman Thoriq Al Farisyi <71012086+salmanthoriq95@users.noreply.github.com>
2021-03-13 23:45:46 +0700

    Create plusShape.c

---
+++ plusShape.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main()
+{
+  int n,i,j;
+  printf("Enter the number of Rows");
+  scanf("%d",&n);
+  for(i=1;i<=n;i++)
+  {
+    for(j=1;j<n;j++)
+    {
+      if( i==n/2 || j==n/2 )
+      printf("0 ");
+      else
+      printf("1 ");
+    }
+    printf("\n");
+  }
+  return 0;
+}
